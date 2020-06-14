0db95516368176c0f9c64adf50bbece640f368ef
Dathremar <mr.protoss@bk.ru>
2020-06-14 15:40:48 +0700

    Pushing Hangman project

--- PycharmProjects/Python Projects/Hangman/Problems/Cook book/task.py
+++
@@ -1,9 +0,0 @@
-pasta = "tomato, basil, garlic, salt, pasta, olive oil"
-apple_pie = "apple, sugar, salt, cinnamon, flour, egg, butter"
-ratatouille = "aubergine, carrot, onion, tomato, garlic, olive oil, pepper, salt"
-chocolate_cake = "chocolate, sugar, salt, flour, coffee, butter"
-omelette = "egg, milk, bacon, tomato, salt, pepper"
-
-ingredient = input()
-
-if 
--- PycharmProjects/Python Projects/Hangman/Problems/Plurals/task.py
+++
@@ -1,8 +0,0 @@
-number = int(input())
-word = input()
-
-if number != 1:
-    word += "s"
-
-
-print(number, word)
