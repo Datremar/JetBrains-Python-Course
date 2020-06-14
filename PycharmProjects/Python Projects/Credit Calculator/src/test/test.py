69c06dc9f67cbc649ec443ab2144e809f5d87f39
Dathremar <mr.protoss@bk.ru>
2020-06-14 15:52:26 +0700

    Removed Credit Calculator folder

--- PycharmProjects/Python Projects/Credit Calculator/src/test/test.py
+++
@@ -1,7 +0,0 @@
-text = input().split()
-
-domains = ('https://', 'http://', 'www.')
-
-for el in text:
-    if el.lower().startswith(domains[0]) or el.lower().startswith(domains[1]) or el.lower().startswith(domains[2]):
-        print(el)
